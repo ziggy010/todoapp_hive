f7d09c711d43008b57b54284112345e284ad3d3f
ARETEE <tajale01@gmail.com>
2022-09-27 08:37:51 +0545

    added slidable

--- lib/components/todotile.dart
+++ lib/components/todotile.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class ToDoTile extends StatelessWidget {
   final String todoText;
   final bool taskCompleted;
   Function(bool?)? onChanged;
-  VoidCallback onDelete;
+  Function(BuildContext)? onDelete;
 
   ToDoTile({
     required this.todoText,
@@ -21,52 +22,51 @@ class ToDoTile extends StatelessWidget {
         horizontal: 32.w,
         vertical: 14.h,
       ),
-      child: Container(
-        width: double.infinity,
-        padding: EdgeInsets.all(
-          8.sm,
-        ),
-        decoration: BoxDecoration(
-          color: Colors.deepPurple.shade700,
-          borderRadius: BorderRadius.circular(
-            8.r,
-          ),
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      child: Slidable(
+        endActionPane: ActionPane(
+          motion: StretchMotion(),
           children: [
-            Row(
-              children: [
-                Checkbox(
-                  value: taskCompleted,
-                  onChanged: onChanged,
-                ),
-                Text(
-                  todoText,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontFamily: 'poppins',
-                    fontSize: 15.sp,
-                    decoration: taskCompleted
-                        ? TextDecoration.lineThrough
-                        : TextDecoration.none,
-                    decorationThickness: 2.0,
-                  ),
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                GestureDetector(
-                  onTap: onDelete,
-                  child: Icon(
-                    (Icons.delete),
-                    color: Color.fromARGB(255, 139, 31, 23),
-                  ),
-                ),
-              ],
+            SlidableAction(
+              onPressed: onDelete,
+              icon: Icons.delete,
+              backgroundColor: Colors.red,
+              borderRadius: BorderRadius.circular(
+                12.r,
+              ),
             ),
           ],
+        ),
+        child: Container(
+          width: double.infinity,
+          padding: EdgeInsets.all(
+            8.sm,
+          ),
+          decoration: BoxDecoration(
+            color: Colors.deepPurple.shade700,
+            borderRadius: BorderRadius.circular(
+              8.r,
+            ),
+          ),
+          child: Row(
+            children: [
+              Checkbox(
+                value: taskCompleted,
+                onChanged: onChanged,
+              ),
+              Text(
+                todoText,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontFamily: 'poppins',
+                  fontSize: 15.sp,
+                  decoration: taskCompleted
+                      ? TextDecoration.lineThrough
+                      : TextDecoration.none,
+                  decorationThickness: 2.0,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
